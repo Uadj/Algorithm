c3b3b21193a493476baaf010be3f242762dc43d5
dgw <30551889+Uadj@users.noreply.github.com>
2021-11-18 04:07:29 +0900

    Create prog_LV3디스크컨트롤러.py

---
+++ prog_LV3디스크컨트롤러.py
@@ -0,0 +1,22 @@
+import heapq;
+
+def solution(jobs):
+    length = len(jobs)
+    now = 0;
+    before = -1;
+    num = 0;
+    arr = []
+    check = 0
+    while check < length:
+        for j in jobs:
+            if before < j[0] <= now:
+                heapq.heappush(arr, [j[1],j[0]])
+        if arr:
+            temp = heapq.heappop(arr)
+            before = now;
+            now += temp[0]
+            num += now-temp[1]
+            check += 1
+        else:
+            now +=-1
+    return int(num/length)
