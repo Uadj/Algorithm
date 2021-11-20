f80ad875b602f817e7094b84e31af14631ea96ee
dgw <30551889+Uadj@users.noreply.github.com>
2021-11-20 09:35:18 +0900

    Update prog_LV3보석쇼핑.py

--- prog_LV3보석쇼핑.py
+++ prog_LV3보석쇼핑.py
@@ -17,7 +17,7 @@ def solution(gems):
         
     return answer
 """
-#타인코드 max heap
+#타인코드 
 def solution(gems):
     gemSet = set()
     for gem in gems:
@@ -25,20 +25,27 @@ def solution(gems):
             gemSet.add(gem)
     kinds = len(gemSet)
     
+    gemDict = {gems[0]: 1}
     answer = [1, len(gems)]
     left = 0; right = 0
     while left <= right and right < len(gems):
-        gemSet = set()
-        for gem in gems[left:right+1]:
-            if gem not in gemSet:
-                gemSet.add(gem)
-        
-        if len(gemSet) == kinds:
+        if len(gemDict) == kinds:
             if answer[1]-answer[0] > right-left:
                 answer = [left+1, right+1]
+            
+            gemDict[gems[left]] -= 1
+            if gemDict[gems[left]] == 0:
+                del gemDict[gems[left]]
             left += 1
 
-        elif len(gemSet) < kinds:
+        elif len(gemDict) < kinds:
             right += 1
+            if right >= len(gems):
+                break
+                
+            if gems[right] not in gemDict:
+                gemDict[gems[right]] = 1
+            else:
+                gemDict[gems[right]] += 1
         
     return answer
